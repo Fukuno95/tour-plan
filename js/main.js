624811eb93824dd9e2328476ba7be14f5602d106
Fukuno95 <sforca13@mail.ru>
2021-01-08 10:57:25 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -41,4 +41,10 @@ ymaps.ready(function () {
     }
   });
 
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function(){
+  console.log("Клик по кнопке меню");
+  document.querySelector(".navbar-bottom").classList.toggle('navbar-bottom--visible');
+});
+
 
